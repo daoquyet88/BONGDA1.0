d09d8bfc2c92768049ceb68ca8cc3a620a6b0b60
Nguyễn Vương <Dragonblue789@gmail.com>
2019-05-30 01:10:14 +0700

    Load thông tin lịch thi đấu

--- BongDa1.0/src/bongda1/pkg0/Themlichthidau.java
+++ BongDa1.0/src/bongda1/pkg0/Themlichthidau.java
@@ -361,7 +361,7 @@ public class Themlichthidau extends javax.swing.JFrame {
         jLabel8 = new javax.swing.JLabel();
         cboTT = new javax.swing.JComboBox();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
